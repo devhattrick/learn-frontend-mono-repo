dccdc17e3cd87a35625fc7f3bc9466c1b3a60f34
Peerapol <peerapol.panmuang@gmail.com>
2024-12-14 16:59:19 +0700

    fix(code): Try method

--- libs/todo/feature/src/lib/todo-feature.component.ts
+++ libs/todo/feature/src/lib/todo-feature.component.ts
@@ -1,7 +1,7 @@
 import { AsyncPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject,signal } from '@angular/core';
 import { TodoService } from '@ats/todo-data-access';
-import { map, shareReplay } from 'rxjs';
+import { delay, map, shareReplay } from 'rxjs';
 
 @Component({
   selector: 'lib-todo-feature',
@@ -11,11 +11,21 @@ import { map, shareReplay } from 'rxjs';
   styleUrl: './todo-feature.component.scss',
 })
 export class TodoFeatureComponent {
+
+  counter = signal(0);
+
   private todoService = inject(TodoService);
+
+
 
   readonly todos$ = this.todoService
     .getTodos()
     .pipe(shareReplay({ refCount: true, bufferSize: 1 }));
+    // .pipe(
+    //   delay(5000),
+    //   map((res)=>{return res})
+    // )
+
 
   readonly totalTodos$ = this.todos$.pipe(map((todos) => todos.length));
 }
